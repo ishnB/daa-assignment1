7ce8d9f2df8962c0ad49e033b5768efd0b5c9597
ishnB <ishan.barpanda74@gmail.com>
2025-03-23 22:24:32 +0530

    frontend added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CliqueAnalysisDashboard from "./home";
+
+function App() {
+  return (
+    <div className="App">
+      <CliqueAnalysisDashboard />
+    </div>
+  );
+}
+
+export default App;
